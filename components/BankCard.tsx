2608a7cc9f7c42bc537c31bbe3aa990ac03af2e5
rmgutierrez <raphael_0810@outlook.com>
2025-02-26 01:27:59 -0700

    update: updated bank card to show real bank info

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,18 +2,19 @@ import { formatAmount } from '@/lib/utils'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true}: CreditCardProps) => {
   return (
     <div className='flex flex-col'>
       <Link 
-        href="/"
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
         className='bank-card'
       >
         <div className='bank-card_content'>
           <div>
             <h1 className='text-16 font-semibold text-white'>
-              {userName}
+              {account.name}
             </h1>
             <p className='font-ibm-plex-serif font-black text-white'>
               {formatAmount(account.currentBalance)}
@@ -31,7 +32,7 @@ const BankCard = ({ account, userName, showBalance = true}: CreditCardProps) => 
             </div>
             <p className='text-14 font-semibold tracking-[2.1px] text-white'>
             ◦◦◦◦ ◦◦◦◦ ◦◦◦◦ <span className='text-16'>
-                1234
+                {account?.mask}
               </span>
             </p>
           </article>
@@ -60,6 +61,9 @@ const BankCard = ({ account, userName, showBalance = true}: CreditCardProps) => 
           className='absolute top-0 left-0'
         />
       </Link>
+      {showBalance && 
+        <Copy title={account?.sharaebleId} />
+      }
     </div>
     
   )
