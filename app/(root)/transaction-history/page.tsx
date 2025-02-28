776c7db56081abc986a3790664dad93f080e1910
rmgutierrez <raphael_0810@outlook.com>
2025-02-27 23:31:23 -0700

    update: reused transaction table in transaction page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,6 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
-import { Pagination } from '@/components/Pagination';
-import TransactionsTable from '@/components/TransactionsTable';
+import RecentTransactions from '@/components/RecentTransactions';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { formatAmount } from '@/lib/utils';
@@ -20,15 +19,6 @@ const TransactionHistory = async ({ searchParams: { id, page }}: SearchParamProp
 
   const account = await getAccount({ appwriteItemId })
 
-  const rowsPerPage = 10;
-  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
-
-  const indexOfLastTransaction = currentPage * rowsPerPage;
-  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
-
-  const currentTransactions = account?.transactions.slice(
-    indexOfFirstTransaction, indexOfLastTransaction
-  );
   return (
     <div className='transactions'>
       <div className='transactions-header'>
@@ -62,17 +52,13 @@ const TransactionHistory = async ({ searchParams: { id, page }}: SearchParamProp
           </div>
         </div>
 
-        <section className='flex w-fill flex-col gap-6'>
-          <TransactionsTable
-            transactions={currentTransactions}
-          />
-
-          {totalPages > 1 && (
-              <div className='my-4 w-full'>
-                <Pagination totalPages={totalPages} page={currentPage}/>
-              </div>
-            )}
-        </section>
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+        
       </div>
     </div>
   )
